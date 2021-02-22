414c4a8f583d6e96a610b6232d956bf786c11527
kudpig <shin.kudo.aa@gmail.com>
2021-02-23 00:16:03 +0900

    [create]HeroViewController

---
+++ JsonData-TableView/HeroViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HeroViewController.swift
+//  JsonData-TableView
+//
+//  Created by Shinichiro Kudo on 2021/02/23.
+//
+
+import UIKit
+
+class HeroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
