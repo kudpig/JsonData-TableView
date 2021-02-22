d919947a7b71268fad88a0cda37b0b76aa9eccef
kudpig <shin.kudo.aa@gmail.com>
2021-02-23 03:30:32 +0900

    [add]imageを取得

--- JsonData-TableView/HeroViewController.swift
+++ JsonData-TableView/HeroViewController.swift
@@ -25,14 +25,38 @@ class HeroViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
         nameLabel.text = hero?.localized_name
         attributeLabel.text = hero?.primary_attr
         attackLabel.text = hero?.attack_type
         legsLabel.text = "\(hero?.legs ?? 0)"
+        
+        // imageのURLを取得
+        let urlString = "https://api.opendota.com"+(hero?.img)!
+        let url = URL(string: urlString)!
+        
+        imageView.downloaded(from: url)
     }
     
+}
 
-    
-
+// サイトから取得:Usage
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { [weak self] in
+                self?.image = image
+            }
+        }.resume()
+    }
+    func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
 }
