d9a2fb4a80c1c8b72561ca824ac1430d11052cba
kudpig <shin.kudo.aa@gmail.com>
2021-02-23 00:22:59 +0900

    [set]VC2_UIパーツの紐付け(IBOutlet)

--- JsonData-TableView/HeroViewController.swift
+++ JsonData-TableView/HeroViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class HeroViewController: UIViewController {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var attributeLabel: UILabel!
+    
+    @IBOutlet weak var attackLabel: UILabel!
+    
+    @IBOutlet weak var legsLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
