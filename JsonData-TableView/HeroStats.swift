8dfa08e7069c29678532368b5fa3f22fcd90a2cf
kudpig <shin.kudo.aa@gmail.com>
2021-02-23 00:52:57 +0900

    [add]デコード用のstructを作成(HeroStats)

--- JsonData-TableView/HeroStats.swift
+++ JsonData-TableView/HeroStats.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+struct HeroStats: Decodable {
+    let localized_name: String
+    let primary_attr: String
+    let attack_type: String
+    let legs: Int
+    let img: String
+}
